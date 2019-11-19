e2184b83fae5b10bd4a34ae47cc59481f5807015
Andre Grevinga <andre.grevinga@gmail.com>
2019-11-19 15:54:10 +0100

    added movement with keys "WASD"

--- FudgeCraft/Main.js
+++ FudgeCraft/Main.js
@@ -1,63 +1,77 @@
 "use strict";
 var FudgeCraft;
 (function (FudgeCraft) {
-    var ƒ = FudgeCore;
+    var f = FudgeCore;
     window.addEventListener("load", hndLoad);
     let viewport;
     let game;
-    let rotate = ƒ.Vector3.ZERO();
+    let rotate = f.Vector3.ZERO();
+    let translate = f.Vector3.ZERO();
     function hndLoad(_event) {
         const canvas = document.querySelector("canvas");
-        ƒ.RenderManager.initialize(true);
-        ƒ.Debug.log("Canvas", canvas);
-        let cmpCamera = new ƒ.ComponentCamera();
-        cmpCamera.pivot.translate(new ƒ.Vector3(2, 3, 10));
-        cmpCamera.pivot.lookAt(ƒ.Vector3.ZERO());
-        game = new ƒ.Node("FudgeCraft");
+        f.RenderManager.initialize(true);
+        f.Debug.log("Canvas", canvas);
+        let cmpCamera = new f.ComponentCamera();
+        cmpCamera.pivot.translate(new f.Vector3(2, 3, 10));
+        cmpCamera.pivot.lookAt(f.Vector3.ZERO());
+        game = new f.Node("FudgeCraft");
         // let cube: Cube = new Cube(CUBE_TYPE.BLUE);
         let fragment = new FudgeCraft.Fragment(0);
         // ƒ.Debug.log("Fragment", fragment);
-        fragment.addComponent(new ƒ.ComponentTransform());
+        fragment.addComponent(new f.ComponentTransform());
         game.appendChild(fragment);
         fragment = new FudgeCraft.Fragment(1);
         // ƒ.Debug.log("Fragment", fragment);
-        fragment.addComponent(new ƒ.ComponentTransform(ƒ.Matrix4x4.TRANSLATION(ƒ.Vector3.X(3))));
+        fragment.addComponent(new f.ComponentTransform(f.Matrix4x4.TRANSLATION(f.Vector3.X(3))));
         game.appendChild(fragment);
         fragment = new FudgeCraft.Fragment(2);
         // ƒ.Debug.log("Fragment", fragment);
-        fragment.addComponent(new ƒ.ComponentTransform(ƒ.Matrix4x4.TRANSLATION(ƒ.Vector3.X(-3))));
+        fragment.addComponent(new f.ComponentTransform(f.Matrix4x4.TRANSLATION(f.Vector3.X(-3))));
         game.appendChild(fragment);
-        let cmpLight = new ƒ.ComponentLight(new ƒ.LightDirectional(ƒ.Color.WHITE));
-        cmpLight.pivot.lookAt(new ƒ.Vector3(0.5, 1, 0.8));
+        let cmpLight = new f.ComponentLight(new f.LightDirectional(f.Color.WHITE));
+        cmpLight.pivot.lookAt(new f.Vector3(0.5, 1, 0.8));
         game.addComponent(cmpLight);
-        viewport = new ƒ.Viewport();
+        viewport = new f.Viewport();
         viewport.initialize("Viewport", game, cmpCamera, canvas);
-        ƒ.Debug.log("Viewport", viewport);
+        f.Debug.log("Viewport", viewport);
         viewport.draw();
-        ƒ.Debug.log("Game", game);
+        f.Debug.log("Game", game);
         window.addEventListener("keydown", hndKeyDown);
     }
     function hndKeyDown(_event) {
         //let rotate: ƒ.Vector3 = ƒ.Vector3.ZERO();
         switch (_event.code) {
-            case ƒ.KEYBOARD_CODE.ARROW_UP:
-                rotate.add(ƒ.Vector3.X(-1));
+            case f.KEYBOARD_CODE.ARROW_UP:
+                rotate.add(f.Vector3.X(-90));
                 break;
-            case ƒ.KEYBOARD_CODE.ARROW_DOWN:
-                rotate.add(ƒ.Vector3.X(1));
+            case f.KEYBOARD_CODE.ARROW_DOWN:
+                rotate.add(f.Vector3.X(90));
                 break;
-            case ƒ.KEYBOARD_CODE.ARROW_LEFT:
-                rotate.add(ƒ.Vector3.Y(-1));
+            case f.KEYBOARD_CODE.ARROW_LEFT:
+                rotate.add(f.Vector3.Y(-90));
                 break;
-            case ƒ.KEYBOARD_CODE.ARROW_RIGHT:
-                rotate.add(ƒ.Vector3.Y(1));
+            case f.KEYBOARD_CODE.ARROW_RIGHT:
+                rotate.add(f.Vector3.Y(90));
+                break;
+            case f.KEYBOARD_CODE.W:
+                translate.add(f.Vector3.Y(1));
+                break;
+            case f.KEYBOARD_CODE.S:
+                translate.add(f.Vector3.Y(-1));
+                break;
+            case f.KEYBOARD_CODE.A:
+                translate.add(f.Vector3.X(-1));
+                break;
+            case f.KEYBOARD_CODE.D:
+                translate.add(f.Vector3.X(1));
                 break;
         }
         for (let fragment of game.getChildren()) {
-            // fragment.cmpTransform.local.rotate(rotate);
+            fragment.cmpTransform.local.translate(translate);
             fragment.cmpTransform.local.rotation = rotate;
         }
-        ƒ.RenderManager.update();
+        translate = f.Vector3.ZERO();
+        f.RenderManager.update();
         viewport.draw();
     }
 })(FudgeCraft || (FudgeCraft = {}));
--- FudgeCraft/Main.ts
+++ FudgeCraft/Main.ts
@@ -1,50 +1,51 @@
 namespace FudgeCraft {
-    import ƒ = FudgeCore;
+    import f = FudgeCore;
 
     window.addEventListener("load", hndLoad);
-    let viewport: ƒ.Viewport;
-    let game: ƒ.Node;
-    let rotate: ƒ.Vector3 = ƒ.Vector3.ZERO();
+    let viewport: f.Viewport;
+    let game: f.Node;
+    let rotate: f.Vector3 = f.Vector3.ZERO();
+    let translate: f.Vector3 = f.Vector3.ZERO();
 
     function hndLoad(_event: Event): void {
         const canvas: HTMLCanvasElement = document.querySelector("canvas");
-        ƒ.RenderManager.initialize(true);
-        ƒ.Debug.log("Canvas", canvas);
+        f.RenderManager.initialize(true);
+        f.Debug.log("Canvas", canvas);
 
-        let cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
-        cmpCamera.pivot.translate(new ƒ.Vector3(2, 3, 10));
-        cmpCamera.pivot.lookAt(ƒ.Vector3.ZERO());
+        let cmpCamera: f.ComponentCamera = new f.ComponentCamera();
+        cmpCamera.pivot.translate(new f.Vector3(2, 3, 10));
+        cmpCamera.pivot.lookAt(f.Vector3.ZERO());
 
-        game = new ƒ.Node("FudgeCraft");
+        game = new f.Node("FudgeCraft");
 
         // let cube: Cube = new Cube(CUBE_TYPE.BLUE);
         let fragment: Fragment = new Fragment(0);
         // ƒ.Debug.log("Fragment", fragment);
-        fragment.addComponent(new ƒ.ComponentTransform());
+        fragment.addComponent(new f.ComponentTransform());
         game.appendChild(fragment);
 
         fragment = new Fragment(1);
         // ƒ.Debug.log("Fragment", fragment);
-        fragment.addComponent(new ƒ.ComponentTransform(ƒ.Matrix4x4.TRANSLATION(ƒ.Vector3.X(3))));
+        fragment.addComponent(new f.ComponentTransform(f.Matrix4x4.TRANSLATION(f.Vector3.X(3))));
         game.appendChild(fragment);
 
         fragment = new Fragment(2);
         // ƒ.Debug.log("Fragment", fragment);
-        fragment.addComponent(new ƒ.ComponentTransform(ƒ.Matrix4x4.TRANSLATION(ƒ.Vector3.X(-3))));
+        fragment.addComponent(new f.ComponentTransform(f.Matrix4x4.TRANSLATION(f.Vector3.X(-3))));
         game.appendChild(fragment);
 
-        let cmpLight: ƒ.ComponentLight = new ƒ.ComponentLight(new ƒ.LightDirectional(ƒ.Color.WHITE));
-        cmpLight.pivot.lookAt(new ƒ.Vector3(0.5, 1, 0.8));
+        let cmpLight: f.ComponentLight = new f.ComponentLight(new f.LightDirectional(f.Color.WHITE));
+        cmpLight.pivot.lookAt(new f.Vector3(0.5, 1, 0.8));
         game.addComponent(cmpLight);
 
 
-        viewport = new ƒ.Viewport();
+        viewport = new f.Viewport();
         viewport.initialize("Viewport", game, cmpCamera, canvas);
-        ƒ.Debug.log("Viewport", viewport);
+        f.Debug.log("Viewport", viewport);
 
         viewport.draw();
 
-        ƒ.Debug.log("Game", game);
+        f.Debug.log("Game", game);
 
         window.addEventListener("keydown", hndKeyDown);
     }
@@ -52,25 +53,37 @@ namespace FudgeCraft {
     function hndKeyDown(_event: KeyboardEvent): void {
         //let rotate: ƒ.Vector3 = ƒ.Vector3.ZERO();
         switch (_event.code) {
-            case ƒ.KEYBOARD_CODE.ARROW_UP:
-                rotate.add(ƒ.Vector3.X(-1));
+            case f.KEYBOARD_CODE.ARROW_UP:
+                rotate.add(f.Vector3.X(-90));
                 break;
-            case ƒ.KEYBOARD_CODE.ARROW_DOWN:
-                rotate.add(ƒ.Vector3.X(1));
+            case f.KEYBOARD_CODE.ARROW_DOWN:
+                rotate.add(f.Vector3.X(90));
                 break;
-            case ƒ.KEYBOARD_CODE.ARROW_LEFT:
-                rotate.add(ƒ.Vector3.Y(-1));
+            case f.KEYBOARD_CODE.ARROW_LEFT:
+                rotate.add(f.Vector3.Y(-90));
                 break;
-            case ƒ.KEYBOARD_CODE.ARROW_RIGHT:
-                rotate.add(ƒ.Vector3.Y(1));
+            case f.KEYBOARD_CODE.ARROW_RIGHT:
+                rotate.add(f.Vector3.Y(90));
+                break;
+            case f.KEYBOARD_CODE.W:
+                translate.add(f.Vector3.Y(1));
+                break;            
+            case f.KEYBOARD_CODE.S:
+                translate.add(f.Vector3.Y(-1));
+                break;
+            case f.KEYBOARD_CODE.A:
+                translate.add(f.Vector3.X(-1));
+                break;
+            case f.KEYBOARD_CODE.D:
+                translate.add(f.Vector3.X(1));
                 break;
         }
         for (let fragment of game.getChildren()) {
-            // fragment.cmpTransform.local.rotate(rotate);
+            fragment.cmpTransform.local.translate(translate);
             fragment.cmpTransform.local.rotation = rotate;
         }
-
-        ƒ.RenderManager.update();
+        translate = f.Vector3.ZERO();
+        f.RenderManager.update();
         viewport.draw();
     }
 }
